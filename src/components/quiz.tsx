02499de732d5f8584934cff565a161a64992908b
william borges <williamborgesaldrighii@gmail.com>
2024-08-07 13:49:50 -0300

    dsds

--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -89,7 +89,7 @@ const Quiz = () => {
   return (
     <div className="w-[95%] flex flex-col justify-center items-center text-center p-8 bg-white rounded-lg shadow-lg relative">
       <h1 className="text-3xl font-bold text-brown mb-6 underline">
-        Quiz Cacau Show
+        Desafio Cacau Show
       </h1>
       {!quizCompleted && (
         <>

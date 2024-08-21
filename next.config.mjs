325b01afa991f011b5c0b66167977754bf14d6d4
william borges <williamborgesaldrighii@gmail.com>
2024-08-21 01:24:23 -0300

    dsds

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self'; script-src 'self' https://www.googletagmanager.com; style-src 'self';",
-          },
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-        ],
-      },
-    ]
-  },
 }
 
 export default nextConfig
